45f76bd3d196f79ad9d440703724d1b48d70b5e8
Mohit Bhatia <mohitbhatia1994@gmail.com>
2015-08-28 00:26:37 -0400

    Update Bookmarklet.js

--- Bookmarklet.js
+++ Bookmarklet.js
@@ -1,5 +1,5 @@
-javascript: (function () { 
-if (!($ = window.jQuery)) { // typeof jQuery=='undefined' works too
+javascript: ( function () { 
+if (!($ = window.jQuery)) { 
     script = document.createElement( 'script' );
    script.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js'; 
     script.onload=insertMaterialCss;
@@ -11,20 +11,13 @@ else {
  
 function insertMaterialCss() {
     var cssCode = document.createElement('link');
-
-cssCode.setAttribute('href','https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.0/css/materialize.min.css');
-cssCode.setAttribute('rel' , 'stylesheet');
-var head = document.getElementsByTagName("head")[0];
-head.insertBefore(cssCode, head.firstChild);
- 
-
+    cssCode.setAttribute('href','https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.0/css/materialize.min.css');
+    cssCode.setAttribute('rel' , 'stylesheet');
+    var head = document.getElementsByTagName("head")[0];
+    head.insertBefore(cssCode, head.firstChild);
     var jsCode = document.createElement('script'); 
     jsCode.setAttribute('src', 'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.0/js/materialize.min.js');                  
-  document.body.appendChild(jsCode);
-  
-
-
-}
- 
+    document.body.appendChild(jsCode);
+    }
  }());
 
